11c58a5b83e2c96d64568eaba7595a75d19567d5
Magnus Hagander <magnus@hagander.net>
2017-10-18 15:59:02 +0200

    Include hit/miss/hitpass counters and size accumulation
    Slightly more interesting monitoring this way, not just pure counting.

--- varnishlog_exporter.go
+++ varnishlog_exporter.go
@@ -9,6 +9,7 @@ import (
 	"github.com/prometheus/common/version"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -19,21 +20,36 @@ var promcounters = prometheus.NewCounterVec(
 		Name: "varnish_custom_counter",
 		Help: "Varnish Custom counters",
 	},
-	[]string{"key"},
+	[]string{"key", "hitmiss"},
+)
+var promcountersizes = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "varnish_custom_size",
+		Help: "Varnish Custom sizes",
+	},
+	[]string{"key", "hitmiss"},
 )
 var promheaders = prometheus.NewCounterVec(
 	prometheus.CounterOpts{
 		Name: "varnish_header_counter",
 		Help: "Varnish Header counters",
 	},
-	[]string{"type", "header", "value"},
+	[]string{"type", "header", "value", "hitmiss"},
+)
+var promheadersizes = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "varnish_header_size",
+		Help: "Varnish Header sizes",
+	},
+	[]string{"type", "header", "value", "hitmiss"},
 )
 
 /* Accumulator goroutine to make sure we can have a small queue */
-func log_accumulator(subchan chan string) {
+func log_accumulator(subchan chan LogCollInfo) {
 	for {
-		key := <-subchan
-		promcounters.With(prometheus.Labels{"key": key}).Inc()
+		lci := <-subchan
+		promcounters.With(prometheus.Labels{"key": lci.logtag, "hitmiss": HITMISS_STRINGS[lci.hitmiss]}).Inc()
+		promcountersizes.With(prometheus.Labels{"key": lci.logtag, "hitmiss": HITMISS_STRINGS[lci.hitmiss]}).Add(lci.size)
 	}
 }
 
@@ -43,10 +59,39 @@ type HeaderInfo struct {
 	value  string
 }
 
-func header_accumulator(subchan chan HeaderInfo) {
+type LogCollInfo struct {
+	logtag  string
+	hitmiss int
+	size    float64
+}
+type HeaderCollInfo struct {
+	headerinfo HeaderInfo
+	hitmiss    int
+	size       float64
+}
+
+const (
+	HITMISS_UNKNOWN    = 0
+	HITMISS_HIT        = 1
+	HITMISS_MISS       = 2
+	HITMISS_HITFORPASS = 3
+	HITMISS_PIPE       = 4
+)
+
+var HITMISS_STRINGS = []string{"UNKNOWN", "HIT", "MISS", "HITFORPASS", "PIPE"}
+
+type SessionCollection struct {
+	hitmiss int
+	size    float64
+	headers []HeaderInfo
+	logtags []string
+}
+
+func header_accumulator(subchan chan HeaderCollInfo) {
 	for {
-		header := <-subchan
-		promheaders.With(prometheus.Labels{"type": header.htype, "header": header.header, "value": header.value}).Inc()
+		hci := <-subchan
+		promheaders.With(prometheus.Labels{"type": hci.headerinfo.htype, "header": hci.headerinfo.header, "value": hci.headerinfo.value, "hitmiss": HITMISS_STRINGS[hci.hitmiss]}).Inc()
+		promheadersizes.With(prometheus.Labels{"type": hci.headerinfo.htype, "header": hci.headerinfo.header, "value": hci.headerinfo.value, "hitmiss": HITMISS_STRINGS[hci.hitmiss]}).Add(hci.size)
 	}
 }
 
@@ -88,6 +133,7 @@ func main() {
 		metricsPath   = flag.String("web.telemetry-path", "/metrics", "Path under which to expose metrics.")
 		varnishName   = flag.String("varnish.name", "", "Name of varnish instance to connect to.")
 		showVersion   = flag.Bool("version", false, "Print version information.")
+		debug         = flag.Bool("debug", false, "Print debugging information (lots!).")
 	)
 	flag.Parse()
 
@@ -101,15 +147,17 @@ func main() {
 	}
 
 	prometheus.MustRegister(promcounters)
+	prometheus.MustRegister(promcountersizes)
 	if has_headers {
 		prometheus.MustRegister(promheaders)
+		prometheus.MustRegister(promheadersizes)
 	}
 
 	// Separate accommulator routine
-	log_subchan := make(chan string, 1000)
+	log_subchan := make(chan LogCollInfo, 1000)
 	go log_accumulator(log_subchan)
 
-	header_subchan := make(chan HeaderInfo, 1000)
+	header_subchan := make(chan HeaderCollInfo, 1000)
 	if has_headers {
 		go header_accumulator(header_subchan)
 	}
@@ -118,6 +166,8 @@ func main() {
 	go httpServer(*listenAddress, *metricsPath)
 
 	for {
+		ctx := SessionCollection{}
+
 		// Open the default Varnish Shared Memory file
 		c := vago.Config{}
 		c.Path = *varnishName
@@ -129,23 +179,57 @@ func main() {
 			continue
 		}
 		v.Log("",
-			vago.RAW,
+			vago.REQ,
 			vago.COPT_TAIL|vago.COPT_BATCH,
 			func(vxid uint32, tag, _type, data string) int {
+				if *debug {
+					fmt.Printf("vxid %d, tag %s, type %s, data %s\n", vxid, tag, _type, data)
+				}
+
+				if tag == "Begin" && strings.HasPrefix(data, "req") {
+					ctx = SessionCollection{}
+				}
+				if tag == "End" {
+					for _, l := range ctx.logtags {
+						log_subchan <- LogCollInfo{l, ctx.hitmiss, ctx.size}
+					}
+					for _, h := range ctx.headers {
+						header_subchan <- HeaderCollInfo{h, ctx.hitmiss, ctx.size}
+					}
+				}
+				if tag == "Hit" {
+					ctx.hitmiss = HITMISS_HIT
+				}
+				if tag == "HitPass" {
+					ctx.hitmiss = HITMISS_HITFORPASS
+				}
+				if tag == "VCL_call" {
+					if data == "MISS" {
+						ctx.hitmiss = HITMISS_MISS
+					}
+					if data == "PIPE" {
+						ctx.hitmiss = HITMISS_PIPE
+					}
+				}
+				if tag == "ReqAcct" {
+					pieces := strings.Split(data, " ")
+					ctx.size, _ = strconv.ParseFloat(pieces[5], 64)
+				}
+
 				if tag == "VCL_Log" && strings.HasPrefix(data, "logkey:") {
 					// the key is the rest of the string after :
-					log_subchan <- data[7:]
+					ctx.logtags = append(ctx.logtags, data[7:])
 				}
 				if has_reqheaders && tag == "ReqHeader" {
 					pieces := strings.SplitN(data, ": ", 2)
 					if _, exists := reqheaders[pieces[0]]; exists {
-						header_subchan <- HeaderInfo{htype: "req", header: pieces[0], value: pieces[1]}
+						ctx.headers = append(ctx.headers, HeaderInfo{htype: "req", header: pieces[0], value: pieces[1]})
 					}
 				}
 				if has_respheaders && tag == "RespHeader" {
 					pieces := strings.SplitN(data, ": ", 2)
 					if _, exists := respheaders[pieces[0]]; exists {
-						header_subchan <- HeaderInfo{htype: "resp", header: pieces[0], value: pieces[1]}
+						ctx.headers = append(ctx.headers, HeaderInfo{htype: "resp", header: pieces[0], value: pieces[1]})
 					}
 				}
 
